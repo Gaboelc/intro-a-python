8f55ce927aa144d35f0b80854a93761481e233da
Gaboelc <gabrielleon917@gmail.com>
2022-02-09 23:45:32 -0600

    Practicas agregadas

---
+++ practicas/001.antecesor_sucesor_num.py
@@ -0,0 +1,5 @@
+num = input()
+
+print("El numero es: ", num)
+print("Antecesor:", int(num)-1)
+print("Sucesor: ", int(num)+1)
---
+++ practicas/002.mayor_de_edad.py
@@ -0,0 +1,5 @@
+edad_mayor = 18
+
+edad_usuario = int(input())
+
+print("Es mayor de edad hace: ", edad_usuario - edad_mayor, " Años")
