185bc9dc4022db1da0f62ca835c07183167bcc3a
Gaboelc <gabrielleon917@gmail.com>
2022-02-09 23:36:55 -0600

    Explicacion Operadores Aritmeticos agregado

---
+++ Operadores_aritmeticos.py
@@ -0,0 +1,36 @@
+"""
++ Suma
+- Resta
+* Multiplicacion
+/ Divicion
+% Modulo
+** Exponte
+// Divicion exacta
+
+Ejemplos:
+"""
+
+A = 10
+B = 5.5
+
+# Suma
+print("Suma:", A+B, "\n")
+
+# Resta
+print("Resta", A-B, "\n")
+
+# Multiplicacion
+print("Multiplicacion", A*B, "\n")
+
+# Divicion
+print("Divicion", A/B, "\n")
+
+# Modulo
+print("Modulo", A%B, "\n")
+
+# Exponte
+print("Exponente", A**B, "\n")
+
+# Divicion exacta
+
+print ("Divicion exacta", A//B, "\n")
